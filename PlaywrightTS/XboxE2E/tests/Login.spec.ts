07d7733d85ed70bd244c12b7b898f652accc1cf9
Codingwith505 <sushilmaurya9192@gmail.com>
2026-06-06 23:55:39 +0530

    add some new testcase in Xboxframework

--- PlaywrightTS/XboxE2E/tests/Login.spec.ts
+++ PlaywrightTS/XboxE2E/tests/Login.spec.ts
@@ -1,3 +1,4 @@
+import { TIMEOUT } from "node:dns";
 import {test,expect} from "./Base"
 
 test('Login xbox web',async({userPage})=>{
@@ -14,5 +15,13 @@ test('Login xbox web',async({userPage})=>{
     await userPage.click('div[aria-label="Use your password"]');
     await userPage.fill('//input[@type="password"]',wrongpass);
     await userPage.click('button[type=submit]');
+    await userPage.on("dialog",dialog => dialog.dismiss());
+    await expect(userPage.locator("div[aria-label=GraveBroom5235]")).toBeVisible({timeout:5000});
+    await userPage.click("div[aria-label=GraveBroom5235]");
+    await expect(userPage.locator("span[data-tid=me-control-displayname]")).toHaveText("GraveBroom5235");
+})
+
+test('purchase the product', async ({userPage})=>{
+
 })
 
