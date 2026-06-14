c1d411b039e5f048f21d07d286633c98fbad1b7b
Codingwith505 <sushilmaurya9192@gmail.com>
2026-06-14 13:15:38 +0530

    add dynamic locator

--- PlaywrightTS/XboxE2E/Pages/CartPage.ts
+++ PlaywrightTS/XboxE2E/Pages/CartPage.ts
@@ -4,10 +4,31 @@ import { BasePage} from './Base'
 export class CartPage extends BasePage{
 
     private readonly clickSearchBar:Locator;
+    private readonly searchBar:Locator;
+    private readonly statusOfGame:Locator;
 
     constructor(page:Page){
         super(page);
+        
         this.clickSearchBar = page.getByTitle("Search xbox.com");
+        this.searchBar = page.getByRole('combobox');
+        this.statusOfGame = page.locator(".module__desktopContainer___F5e");
+    }
+
+    private getSelectResult(gameName:string):Locator{
+            return this.page.getByAltText(gameName);
+        }
+
+    async searchGame(gameName:string){
+        await this.clickSearchBar.click();
+        await this.searchBar.fill(gameName);
+        await this.getSelectResult(gameName).click();
+
+    }
+
+    async getTextResult(){
+        await this.statusOfGame.click();
+
     }
 
 
