52b892c4179f5d7826e6e30fa2cf241689375b22
Codingwith505 <sushilmaurya9192@gmail.com>
2026-06-25 21:21:25 +0530

    adding navBar page

---
+++ PlaywrightTS/XboxE2E/Components/NavBar.ts
@@ -0,0 +1,15 @@
+import {expect,Page, Locator} from '@playwright/test';
+import { BasePage} from '../Pages/Base';
+
+
+export class navBar extends BasePage{
+
+
+
+    constructor(page:Page){
+        super(page)
+
+        
+    }
+
+}
