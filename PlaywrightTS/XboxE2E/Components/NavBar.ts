a8319c70d6cb48502a4d8b48f77a4c68c89a71c7
Codingwith505 <sushilmaurya9192@gmail.com>
2026-06-26 23:20:12 +0530

    adding navbar class

--- PlaywrightTS/XboxE2E/Components/NavBar.ts
+++ PlaywrightTS/XboxE2E/Components/NavBar.ts
@@ -4,12 +4,36 @@ import { BasePage} from '../Pages/Base';
 
 export class navBar extends BasePage{
 
+    private readonly gamePass:Locator;
+    private readonly games:Locator;
+    private readonly devices:Locator;
+    private readonly store:Locator;
+    private readonly privacy:Locator;
+    private readonly community:Locator;
+    private readonly support:Locator;
+    private readonly developers:Locator;
+
+
 
 
     constructor(page:Page){
         super(page)
 
         
+
+
     }
+
+}
+
+export class gamePass extends navBar{
+
+    
+
+}
+
+export class games extends navBar{
+
+    
 
 }
